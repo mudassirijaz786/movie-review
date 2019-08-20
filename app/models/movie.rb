79b39e09091e660a3ef9d7019d8040e6224466bc
mudassir-ijaz <ijazmudassir786@gmail.com>
2019-08-20 20:09:03 +0500

    second commit, devise confirmation, mailcatcher, genre dropdown

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,5 +6,6 @@ class Movie < ApplicationRecord
     validates :title,:director_name,:director_dob, :actor_name, :actor_dob, :genra, :year, :image, :presence => true
 
     has_attached_file :image, styles: { medium: "400x600#" }, default_url: "/images/:style/missing.png"
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    CATEGORY = ["Drama", "Comedy", "Action", "Adventure", "Horror"]
 end
